07e316a00bdcfca29ca0129b3caca00ca4f646ed
Dijedon <99071957+dijedontahiri@users.noreply.github.com>
2026-09-23 18:48:14 +0200

    test(db-postgres): add block relationship query fixture

---
+++ test/relationships-block-path/config.ts
@@ -0,0 +1,46 @@
+import { buildConfigWithDefaults } from '../buildConfigWithDefaults.js'
+
+export default buildConfigWithDefaults({
+  collections: [
+    {
+      slug: 'documents',
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+        },
+      ],
+    },
+    {
+      slug: 'pages',
+      fields: [
+        {
+          name: 'layout',
+          type: 'blocks',
+          blocks: [
+            {
+              slug: 'textBlock',
+              fields: [
+                {
+                  name: 'text',
+                  type: 'text',
+                },
+              ],
+            },
+            {
+              slug: 'documentList',
+              fields: [
+                {
+                  name: 'documents',
+                  type: 'relationship',
+                  relationTo: 'documents',
+                  hasMany: true,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+})
